909563b0b82e45f149d30ce826442e9e09c6d2f7
Geraldo Reis <geraldo.reis@chaordicsystems.com>
2020-10-03 23:13:26 -0300

    Fix path github.io

--- src/index.js
+++ src/index.js
@@ -26,11 +26,11 @@ function getToRender () {
 function renderOneLine(name, fontName, imageQuerySelector) {
     const imageElement = document.querySelector(imageQuerySelector);
     if (imageElement) {
-        Jimp.read('dist/img/unnamed.png')
+        Jimp.read('watermark-browser/dist/img/unnamed.png')
         // Jimp.read('/img/untitled.png')
             .then(async unnamed => {
-                const nameFont = await Jimp.loadFont('dist/fonts/lato-italic/'+fontName+'.fnt')
-                const rankFont = await Jimp.loadFont('dist/fonts/lato-italic/lato-italic-12.fnt')
+                const nameFont = await Jimp.loadFont('watermark-browser/dist/fonts/lato-italic/'+fontName+'.fnt')
+                const rankFont = await Jimp.loadFont('watermark-browser/dist/fonts/lato-italic/lato-italic-12.fnt')
 
                 // console.log('measureText name', Jimp.measureText(nameFont, name))
                 // console.log('measureTextHeight name', Jimp.measureTextHeight(nameFont, name))
@@ -76,11 +76,11 @@ function renderOneLine(name, fontName, imageQuerySelector) {
 function renderTwoLine(firstLine, lastLine, fontName, imageQuerySelector) {
     const imageElement = document.querySelector(imageQuerySelector);
     if (imageElement) {
-        Jimp.read('dist/img/unnamed.png')
+        Jimp.read('watermark-browser/dist/img/unnamed.png')
         // Jimp.read('/img/untitled.png')
             .then(async unnamed => {
-                const nameFont = await Jimp.loadFont('dist/fonts/lato-italic/'+fontName+'.fnt')
-                const rankFont = await Jimp.loadFont('dist/fonts/lato-italic/lato-italic-12.fnt')
+                const nameFont = await Jimp.loadFont('watermark-browser/dist/fonts/lato-italic/'+fontName+'.fnt')
+                const rankFont = await Jimp.loadFont('watermark-browser/dist/fonts/lato-italic/lato-italic-12.fnt')
 
                 // console.log('measureText name', Jimp.measureText(nameFont, name))
                 // console.log('measureTextHeight name', Jimp.measureTextHeight(nameFont, name))
